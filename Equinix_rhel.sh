e21bfbf85bbfa33fb58cbb86080743057fbc5f59
richardwilkins999 <157890680+richardwilkins999@users.noreply.github.com>
2024-02-02 00:42:16 +0800

    Create Equinix_rhel.sh

---
+++ Equinix_rhel.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+yum -y install tigervnc-server tigervnc
+yum -y group install GNOME base-x
+yum -y groupinstall "Server with GUI"
+systemctl set-default graphical.target
+systemctl isolate graphical.target
+
+
+#Create the User Env
+useradd vncuser1
+#passwd vnc
+echo 'session=gnome' > /home/vncuser1/.vnc/config
+echo ':1=vncuser1' >> /etc/tigervnc/vncserver.users
+cp /lib/systemd/system/vncserver@.service /etc/systemd/system/vncserver@:1.service
+
+#set the password
+#mkdir -p /home/vncuser1/.vnc && x11vnc -storepasswd admin /home.vncuser1/.vnc/passwd
+
+#open the ports
+firewall-cmd --permanent --zone=public --add-port 5901/tcp
+firewall-cmd  --reload
+
+
+
+
+
