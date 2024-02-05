8ed6ca9dc8cf094d89fb83efad2fa87af7776a4f
richardwilkins999 <157890680+richardwilkins999@users.noreply.github.com>
2024-02-06 01:45:24 +0800

    Update Equinix_Ubuntu_VirtMgr.sh

--- Equinix_Ubuntu_VirtMgr.sh
+++ Equinix_Ubuntu_VirtMgr.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-# The userid who will run VNC
+# The userid and password for who will run VNC
 read -p "Please enter the UserID for VNC:" TARGET_USER
+read -p "Please enter the Password for $TARGET_USER:" TARGET_USER_PASSWORD
+
+#Create VNCUser and set password
+useradd -m $TARGET_USER && echo "${TARGET_USER}:${TARGET_USER_PASSWORD}" | chpasswd
 
 #Interface Name
 IF_NAME="bond0"
@@ -14,6 +18,38 @@ if [ -z "$VLAN_ID" ]; then
     echo "VLAN ID is required. Exiting."
     exit 1
 fi
+
+## Install VNC
+#run update
+apt -y upgrade
+apt -y update
+
+#Install TigerVNC   ** Not tightvnc **
+apk add bash curl jq openssl sudo nano git pciutils gzip p7zip cpio tar unzip xarchiver ethtool
+apt -y install tigervnc-standalone-server tigervnc-xorg-extension tigervnc-viewer
+apt -y install ubuntu-gnome-desktop gnome-session gnome-terminal
+#systemctl enable gdm
+#systemctl start gdm
+
+#set VNC Env
+mkdir -p /home/$TARGET_USER/.vnc
+chown $TARGET_USER:$TARGET_USER /home/$TARGET_USER/.vnc
+
+#Create the VNC config
+cat > /home/$TARGET_USER/.vnc/xstartup <<EOF
+#!/bin/sh
+/usr/bin/gnome-session
+EOF
+
+chmod +x /home/$TARGET_USER/.vnc/xstartup
+
+#open the ports
+echo "Opening FW Ports"
+ufw allow 5901/tcp
+ufw allow 5902/tcp
+ufw reload
+
+### install VirtMgr
 
 #Just to be sure
 apt -y update
@@ -29,7 +65,7 @@ systemctl enable --now libvirtd
 #install Virt-Mgt
 apt install -y virt-manager
 
-#Make Creat the VNC config
+#Make Creat the VLAN config
 cat > /etc/network/interfaces <<EOF
 #!/bin/sh
 #create VLAN
@@ -39,5 +75,17 @@ EOF
 
 #Load the VLAN
 systemctl restart networking.service
+
+#print public IP address
+apt -y install jq
+PUBLIC_IP=$(curl -s https://metadata.platformequinix.com/metadata | jq -r ".network.addresses[] | select(.public == true) | select(.address_family == 4) | .address")
+echo "Public IP is : $PUBLIC_IP"
+
+#Do not Start the VNCServer, in the script
+echo "Please start VNC manually and set the VNCPassword"
+echo "su - $TARGET_USER"
+echo "vncserver"
+
+echo "Also please reboot your server"
 
 exit
