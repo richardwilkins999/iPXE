e773d27d3c7472132591c6687ae7ee090b1d447f
richardwilkins999 <157890680+richardwilkins999@users.noreply.github.com>
2024-02-05 21:59:22 +0800

    Update Equinix_RHEL_VBox.sh

--- Equinix_RHEL_VBox.sh
+++ Equinix_RHEL_VBox.sh
@@ -10,4 +10,6 @@ rpm --import oracle_vbox.asc
 
 #Install configure the Repo
 dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+
+echo "Installing VBOX"
 dnf install -y VirtualBox
