3dd12d7a180267fb3520040e50d027052b669de8
richardwilkins999 <157890680+richardwilkins999@users.noreply.github.com>
2024-02-05 23:29:21 +0800

    Update Equinix_Ubuntu_VNC.sh

--- Equinix_Ubuntu_VNC.sh
+++ Equinix_Ubuntu_VNC.sh
@@ -6,7 +6,8 @@ TARGET_USER="vncuser1"
 TARGET_USER_PASSWORD="snaresnare"
 
 #Create VNCUser
-useradd -m $TARGET_USER && echo '$TARGET_USER:$TARGET_USER_PASSWORD' | chpasswd
+#useradd -m $TARGET_USER && echo '$TARGET_USER:$TARGET_USER_PASSWORD' | chpasswd
+useradd -m $TARGET_USER && echo "${TARGET_USER}:${TARGET_USER_PASSWORD}" | chpasswd
 
 #run update
 apt -y update
