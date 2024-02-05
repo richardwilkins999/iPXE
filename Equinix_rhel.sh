bc706690a04f139022612c0de41d1b15f519c8c7
richardwilkins999 <157890680+richardwilkins999@users.noreply.github.com>
2024-02-05 19:23:06 +0800

    Update Equinix_rhel.sh

--- Equinix_rhel.sh
+++ Equinix_rhel.sh
@@ -1,28 +1,46 @@
 #!/bin/sh
 
+# The username for whom to set the VNC password
+TARGET_USER="vncuser1"
+TARGET_USER_PASSWORD="admin"
+
+# The desired password
+VNC_PASSWORD="snaresnare"
+
+# Create a temporary script to set the VNC password
+TMP_SCRIPT=$(mktemp)
+
 yum -y install tigervnc-server tigervnc
 yum -y group install GNOME base-x
 yum -y groupinstall "Server with GUI"
 systemctl set-default graphical.target
 systemctl isolate graphical.target
 
-
 #Create the User Env
-useradd vncuser1
-
-#passwd vnc
-
+useradd -m $TARGET_USER && echo '$TARGET_USER:$TARGET_USER_PASSWORD' | sudo chpasswd
 
 #set VNC Env
-mkdir -p /home/vncuser1/.vnc
-chown vncuser1:vncuser1 /home/vncuser1/.vnc
-touch /home/vncuser1/.vnc/config
-echo 'session=gnome' > /home/vncuser1/.vnc/config
-echo ':1=vncuser1' >> /etc/tigervnc/vncserver.users
+mkdir -p /home/$TARGET_USER/.vnc
+chown $TARGET_USER:$TARGET_USER /home/$TARGET_USER/.vnc
+touch /home/$TARGET_USER/.vnc/config
+echo 'session=gnome' > /home/$TARGET_USER/.vnc/config
+echo ':1=$TARGET_USER' >> /etc/tigervnc/vncserver.users
 cp /lib/systemd/system/vncserver@.service /etc/systemd/system/vncserver@:1.service
 
-#set the password
-#mkdir -p /home/vncuser1/.vnc && x11vnc -storepasswd admin /home.vncuser1/.vnc/passwd
+# Create a temporary script to set the VNC password
+TMP_SCRIPT=$(mktemp)
+
+echo "#!/bin/bash
+echo \$VNC_PASSWORD | vncpasswd" > "$TMP_SCRIPT"
+
+# Make the temporary script executable
+chmod +x "$TMP_SCRIPT"
+
+# Execute the script as the target user
+sudo -u "$TARGET_USER" VNC_PASSWORD="$VNC_PASSWORD" "$TMP_SCRIPT"
+
+# Remove the temporary script
+rm "$TMP_SCRIPT"
 
 #open the ports
 firewall-cmd --permanent --zone=public --add-port 5901/tcp
