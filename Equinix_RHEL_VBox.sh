1d0c3e2e1afd1a841f94095760be0a1e2af582d9
richardwilkins999 <157890680+richardwilkins999@users.noreply.github.com>
2024-02-05 21:51:23 +0800

    Create Equinix_RHEL_VBox.sh

---
+++ Equinix_RHEL_VBox.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#Download the Repo defination
+wget https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo
+mv virtualbox.repo /etc/yum.repos.d/
+
+#Download the Oracle Keys
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc
+rpm --import oracle_vbox.asc
+
+#Install configure the Repo
+dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+
