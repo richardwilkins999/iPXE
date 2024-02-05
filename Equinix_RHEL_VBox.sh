6f6280bdfba7037cba5197b73c45792d25774e29
richardwilkins999 <157890680+richardwilkins999@users.noreply.github.com>
2024-02-06 01:30:14 +0800

    Update Equinix_RHEL_VBox.sh

--- Equinix_RHEL_VBox.sh
+++ Equinix_RHEL_VBox.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # The userid who will run VNC
-read -p "Please enter the UserID for VNC:" TARGET_USER
+read -p "Please enter the UserID for VNC : " TARGET_USER
 
-# Ask user Password
-read -p "Please enter the Password for $TARGET_USER:" TARGET_USER_PASSWORD
+# Ask user Password for VNC User
+read -p "Please enter the Password for $TARGET_USER : " TARGET_USER_PASSWORD
 
 #Interface Name
 IF_NAME="bond0"
@@ -17,6 +17,32 @@ if [ -z "$VLAN_ID" ]; then
     echo "VLAN ID is required. Exiting."
     exit 1
 fi
+
+### Install VNC
+yum -y install tigervnc-server tigervnc
+yum -y group install GNOME base-x
+yum -y groupinstall "Server with GUI"
+systemctl set-default graphical.target
+systemctl isolate graphical.target
+
+#Create the User Env
+echo "Creating user:"$TARGET_USER
+useradd -m $TARGET_USER && echo '$TARGET_USER:$TARGET_USER_PASSWORD' | chpasswd
+
+#set VNC Env
+mkdir -p /home/$TARGET_USER/.vnc
+chown $TARGET_USER:$TARGET_USER /home/$TARGET_USER/.vnc
+touch /home/$TARGET_USER/.vnc/config
+echo 'session=gnome' > /home/$TARGET_USER/.vnc/config
+echo ':1=$TARGET_USER' >> /etc/tigervnc/vncserver.users
+cp /lib/systemd/system/vncserver@.service /etc/systemd/system/vncserver@:1.service
+
+#open the ports
+echo "Opening FW Ports"
+firewall-cmd --permanent --zone=public --add-port 5901/tcp
+firewall-cmd  --reload
+
+#### Install VBOX
 
 #Download the Repo defination
 wget https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo
@@ -48,11 +74,6 @@ modprobe 8021q
 touch /etc/modules-load.d/8021q.conf
 echo "8021q" | tee /etc/modules-load.d/8021q.conf
 
-
-echo "ip link add link $IF_NAME name $IF_NAME.$VLAN_ID type vlan id $VLAN_ID"
-echo "ip link set dev $IF_NAME.$VLAN_ID up"
-
-
 ip link add link $IF_NAME name $IF_NAME.$VLAN_ID type vlan id $VLAN_ID
 ip link set dev $IF_NAME.$VLAN_ID up
 
@@ -64,6 +85,13 @@ ONBOOT=yes
 VLAN=yes
 EOF
 
+#removing for now
+dnf -y update
+dnf -y upgrade
+
+#Reboot after config
+#systemctl reboot
+
 #print public IP address
 dnf install -y jq
 PUBLIC_IP=$(curl -s https://metadata.platformequinix.com/metadata | jq -r ".network.addresses[] | select(.public == true) | select(.address_family == 4) | .address")
@@ -71,9 +99,10 @@ echo "Public IP is : $PUBLIC_IP"
 
 #Please manually install the VBOX extensions
 echo "VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-*.vbox-extpack"
+echo ""
 
-#removing for now
-#dnf -y update
-#dnf -y upgrade
-#systemctl reboot
+#Do not Start the VNCServer, in the script
+echo "Please start VNC manually and set the VNCPassword by running the following commands"
+echo "su - $TARGET_USER"
+echo "vncserver"
 
