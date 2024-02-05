333d87a5ffdee97c896be10d1b4dab7ea136bfc1
richardwilkins999 <157890680+richardwilkins999@users.noreply.github.com>
2024-02-06 00:03:55 +0800

    Update Equinix_Ubuntu_VNC.sh

--- Equinix_Ubuntu_VNC.sh
+++ Equinix_Ubuntu_VNC.sh
@@ -13,9 +13,10 @@ apt -y update
 
 #Install TigerVNC   ** Not tightvnc **
 apt install -y tigervnc-standalone-server tigervnc-xorg-extension tigervnc-viewer
-apt install -y ubuntu-gnome-desktop
-systemctl enable gdm
-systemctl start gdm
+apt install -y xfce4 xfce4-goodies
+#apt install -y ubuntu-gnome-desktop
+#systemctl enable gdm
+#systemctl start gdm
 
 #set VNC Env
 mkdir -p /home/$TARGET_USER/.vnc
@@ -24,11 +25,17 @@ chown $TARGET_USER:$TARGET_USER /home/$TARGET_USER/.vnc
 #Make Creat the VNC config
 cat > /home/$TARGET_USER/.vnc/xstartup <<EOF
 #!/bin/sh
-export XKL_XMODMAP_DISABLE=1
-export XDG_CURRENT_DESKTOP="GNOME-Flashback:GNOME"
-export XDG_MENU_PREFIX="gnome-flashback-"
-gnome-session --session=gnome-flashback-metacity --disable-acceleration-check &
+# Start up the standard system desktop
+unset SESSION_MANAGER
+unset DBUS_SESSION_BUS_ADDRESS
+/usr/bin/startxfce4
+[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+x-window-manager &
 EOF
+
+chmod +x /home/$TARGET_USER/.vnc/xstartup
+
 
 #run update again  ?:-)
 apt -y update
@@ -36,6 +43,7 @@ apt -y update
 #open the ports
 echo "Opening FW Ports"
 ufw allow 5901/tcp
+ufw reload
 
 #Do not Start the VNCServer, in the script
 echo "Please start VNC manually and set the VNCPassword"
